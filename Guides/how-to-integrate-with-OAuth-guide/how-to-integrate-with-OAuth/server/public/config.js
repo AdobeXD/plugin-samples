24a89855b4bfb139ee82714846c64721bd23d203
Steve Kwak <kwak@adobe.com>
2018-08-01 18:18:34 -0400

    dropbox apikey and secret removed

--- Guides/how-to-integrate-with-OAuth-guide/how-to-integrate-with-OAuth/server/public/config.js
+++ Guides/how-to-integrate-with-OAuth-guide/how-to-integrate-with-OAuth/server/public/config.js
@@ -1,9 +1,9 @@
 /* Replace "YOUR_ADOBE_API_KEY" with your Api key 
 and "YOUR_ADOBE_API_SECRET" with your API Secret */
 
-const dropboxApiKey = "y87386c2j2m2x89";
-const dropboxApiSecret = "gf995bqmhgdcqzv";
-const publicUrl = "https://176d0d74.ngrok.io";
+const dropboxApiKey = "YOUR-DROPBOX-APIKEY";
+const dropboxApiSecret = "YOUR-DROPBOX-SECRET";
+const publicUrl = "YOUR-PUBLIC-URL";
 
 try {
         if (module) {
