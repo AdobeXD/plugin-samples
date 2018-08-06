3978b00b3eba0e362dd1bce9cddbbffc9a9a7861
Kris Nye <knye@adobe.com>
2018-08-06 09:10:00 -0700

    Fixed hyperscript sample

--- ui-button-padding-hyperscript/src/main.js
+++ ui-button-padding-hyperscript/src/main.js
@@ -1,45 +1,49 @@
 const h = require("hyperscript")
 
-function onsubmit(e) {
-    let values = {
-        horizontal: horizontal.value,
-        vertical: vertical.value
-    };
-    console.log("submit: " + JSON.stringify(values));
-    dialog.close();
-}
-
-let horizontal, vertical;
-let dialog =
-    h("dialog",
-        "Hello Hyperscript",
-        h("form", { style: { width: "360px" }, onsubmit },
-            h("h1", "Button Padding"),
-            h("p", `Enter your button's vertical and horizontal padding and then click "Update Padding".`),
-            h("div.row",
-                h("label",
-                    h("span", "Horizontal"),
-                    horizontal = h("input", { value: "20px" })
-                ),
-                h("label",
-                    h("span", "Vertical"),
-                    vertical = h("input", { value: "20px" })
+let dialog;
+function getDialog() {
+    if (dialog == null) {
+        function onsubmit(e) {
+            let values = {
+                horizontal: horizontal.value,
+                vertical: vertical.value
+            };
+            console.log("submit: " + JSON.stringify(values));
+            dialog.close();
+        }
+        
+        let horizontal, vertical;
+        dialog =
+            h("dialog",
+                h("form", { onsubmit },
+                    h("h1", "Button Padding"),
+                    h("p", `Enter padding values and then click "Update Padding".`),
+                    h("div.row",
+                        h("label",
+                            h("span", "Horizontal"),
+                            horizontal = h("input", { value: "20px" })
+                        ),
+                        h("label",
+                            h("span", "Vertical"),
+                            vertical = h("input", { value: "20px" })
+                        )
+                    ),
+                    h("footer",
+                        h("button", { uxpVariant: "primary", onclick(e) { dialog.close("Cancelled") } }, "Cancel"),
+                        h("button", { type:"submit", uxpVariant: "cta", onclick(e) { onsubmit(e); e.preventDefault(); } }, "Update Padding")
+                    )
                 )
-            ),
-            h("footer",
-                h("button", { uxpVariant: "primary", onclick(e) { dialog.close("Cancelled") } }, "Cancel"),
-                h("button", { type:"submit", uxpVariant: "cta", onclick(e) { onsubmit(e); e.preventDefault(); } }, "Update Padding")
             )
-        )
-    )
-document.body.appendChild(dialog);
+        // temporary work-around since styles don't currently work with hyperscript due to lack of CSSStyleDeclaration.prototype.setProperty function.
+        dialog.style.width = 350;
+    }
+    return dialog
+}
 
 module.exports = {
     commands: {
         menuCommand: function () {
-            dialog.showModal().then(reason => {
-                console.log("REASON: " + reason);
-            });
+            document.body.appendChild(getDialog()).showModal()
         }
     }
 };
