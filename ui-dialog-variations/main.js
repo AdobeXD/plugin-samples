29b27e5f5d7d4832ba5007808dd0f76528722ef1
Kerri Shotts <shotts@adobe.com>
2018-08-27 13:53:08 -0400

    Updated wording; added destructive example

--- ui-dialog-variations/main.js
+++ ui-dialog-variations/main.js
@@ -47,7 +47,6 @@ async function notice({title, icon, msgs, prompt, buttons=[{label: "Close", vari
         prompt ?
             `
                 <label>
-                    <span>${prompt}</span>
                     <input type="text" id="prompt" placeholder="${prompt}" />
                 </label>
             `
@@ -103,7 +102,7 @@ async function alert(title, ...msgs) {
  * @param {*} title
  * @param {*} msgs
  */
-async function warning(title, ...msgs) {
+async function error(title, ...msgs) {
     return notice({title, icon: "alert-red", msgs});
 }
 
@@ -111,6 +110,13 @@ async function confirm(title, msg, buttons = [ "Cancel", "OK" ]) {
     return notice({title, msgs: [msg], buttons: [
         {label: buttons[0], type:"reset", variant: "primary"},
         {label: buttons[1], type:"submit", variant: "cta"}
+    ]});
+}
+
+async function warning(title, msg, buttons = [ "Cancel", "OK" ]) {
+    return notice({title, msgs: [msg], buttons: [
+        {label: buttons[0], type:"submit", variant: "primary"},
+        {label: buttons[1], type:"button", variant: "warning"}
     ]});
 }
 
@@ -125,38 +131,53 @@ function about() {
 }
 
 async function showAlert() {
-    return alert("Uploaded successfully!",
-                 "Your changes were uploaded successfully.");
+    return alert("Connect to the Internet",
+                 "In order to function correctly, this plugin requires Internet access. Please connect to a network that has Internet access.");
 }
 
 async function showConfirm() {
-    const feedback = await confirm("Ask for design feedback?",
-                   "Indicate if you want this plugin to provide you with design feedback.",
-                ["I'm a coward", "Bring it on"]);
+    const feedback = await confirm("Enable Smart Filters?",
+                   "Smart filters are nondestructive and will preserve your original images.",
+                ["Cancel", "Enable"]);
     switch (feedback.which) {
         case 0:
-            return alert("Awwww!", "I was going to say nice things. Honest!")
+            return alert("Smart Filters", "You clicked 'Cancel'");
+            break;
         case 1:
-            return alert("You asked for it...", "I think it's wonderful!");
+            return alert("Smart Filters Enabled", "You clicked 'Enable'");
             break;
     }
 }
 
 async function showPrompt() {
-    const res = await prompt("What's your name?", "I'm a friendly plugin. I'd like to get to know you!", "Name");
+    const res = await prompt("Create Shape", "What kind of shape you would like to create?", "Shape");
     switch (res.which) {
         case 0:
-            return alert("Awwww.", "Oh well. Maybe another day");
+            return alert("Shape to Create", "You didn't want to apply a shape.");
             break;
         case 1:
-            return alert("Nice to meet you", `I'm happy to meet you, ${res.value}!`);
+            return alert("Shape to Create", `You wanted to create a ${res.value}!`);
             break;
     }
 }
 
+async function showWarning() {
+    const res = await warning("Reset Preferences?",
+                 "Are you sure you want to reset your plugin preferences?", ["Cancel", "Reset Preferences"]);
+    switch (res.which) {
+        case 0:
+            return alert("Preferences Preserved", "Your preferences are safe.");
+            break;
+        case 1:
+            return alert("Preferences Reset", `Your preferences have been reset.`);
+            break;
+    }
+
+}
+
 async function showError() {
-    return warning("Couldn't divide by zero",
-        "An attempt was made to divide by zero. Unfortunately, that operation is undefined.");
+    return error("Couldn't save your changes",
+        "Your computer reports that you are out of storage space. Please move or remove some files to free up space and try again.");
 }
 
 async function showAbout() {
@@ -170,6 +191,7 @@ async function showAbout() {
 module.exports = {
     commands: {
         showAlert,
+        showWarning,
         showError,
         showConfirm,
         showPrompt,
