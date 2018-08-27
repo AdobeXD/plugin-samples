ef4c7ff9362ce310041b2fa7b6ee881f0fd61eea
Kerri Shotts <shotts@adobe.com>
2018-08-27 15:45:32 -0400

    tweaks

--- ui-dialog-variations/main.js
+++ ui-dialog-variations/main.js
@@ -7,38 +7,73 @@
  *
  ******************************************************************************/
 
-/**
+/*
  * Generates a "notice" dialog with the title, default icon, and a series of messages.
  *
- * @param {string} title
- * @param {string} [icon="info-filled"]
- * @param {string[]} msgs
+ * @param {*} param
+ * @property {string} param.title The dialog title
+ * @property {string} [param.icon] The dialog icon to use. If not provided, no icon will be rendered
+ * @property {string[]} param.msgs The messages to render. If a message starts with `http`, it will be rendered as a link.
+ * @property {string} [param.prompt] If specified, will render as a prompt with a single input field and the prompt as a placeholder
+ * @property {boolean} [param.isError=false] If specified, will render the header in a red color
+ * @property {Object[]} [buttons] Indicates the buttons to render. If none are specified, a `Close` button is rendered.
+ * @returns {Promise} Resolves to an object of the form {which, value}. `value` only makes sense if `prompt` is set. `which` indicates which button was pressed.
  */
-async function notice({title, icon, msgs, prompt, buttons=[{label: "Close", variant: "cta", type:"submit"}]} = {}) {
+async function notice({
+    title,
+    icon,
+    msgs,
+    prompt,
+    isError=false,
+    buttons=[
+        {label: "Close", variant: "cta", type:"submit"}
+    ]} = {},
+    width=360,
+    iconSize=14
+) {
+
+    if (icon === 'app-icon') {
+        try {
+            const fs = require("uxp").storage.localFileSystem;
+            const dataFolder = await fs.getPluginFolder();
+            const manifestFile = await dataFolder.getEntry("manifest.json");
+            if (manifestFile) {
+                const json = await manifestFile.read();
+                const manifest = JSON.parse(json);
+                const iconName = manifest.icon;
+                if (iconName) {
+                    icon = iconName;
+                    iconSize = 32;
+                }
+            }
+        } catch (err) {
+            // do nothing
+        }
+    }
+
     const dialog = document.createElement("dialog");
     dialog.innerHTML = `
 <style>
     form {
-        width: 360px;
+        width: ${width}px;
     }
     form h1 {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-    }
-    form h1 span {
+        ${isError ? 'color: #D7373F' : ''}
     }
     form h1 img {
-        width: 14px;
-        height: 14px;
-        flex: 0 0 14px;
+        width: ${iconSize}px;
+        height: ${iconSize}px;
+        flex: 0 0 ${iconSize}px;
     }
 </style>
 <form method="dialog">
     <h1>
         <span>${title}</span>
-        ${icon ? `<img src="assets/${icon}.png" />` : ""}
+        ${icon ? `<img src="${icon}" />` : ""}
     </h1>
     ${
         msgs.map(msg => msg.substr(0, 4) === "http" ? `<a href="${msg}">${msg}</a>` : `<p>${msg}</p>`).join("")
@@ -96,6 +131,10 @@ async function alert(title, ...msgs) {
     return notice({title, msgs});
 }
 
+async function alertWithAppIcon(title, ...msgs) {
+    return notice({icon: "app-icon", title, msgs});
+}
+
 /**
  * Generates a warning message
  *
@@ -103,7 +142,7 @@ async function alert(title, ...msgs) {
  * @param {*} msgs
  */
 async function error(title, ...msgs) {
-    return notice({title, icon: "alert-red", msgs});
+    return notice({title, isError: true, icon: "assets/alert-red.png", msgs});
 }
 
 async function confirm(title, msg, buttons = [ "Cancel", "OK" ]) {
@@ -125,9 +164,6 @@ async function prompt(title, msg, prompt, buttons = [ "Cancel", "OK" ]) {
         {label: buttons[0], type:"reset", variant: "primary"},
         {label: buttons[1], type:"submit", variant: "cta"}
     ]});
-}
-
-function about() {
 }
 
 async function showAlert() {
@@ -181,7 +217,7 @@ async function showError() {
 }
 
 async function showAbout() {
-    return alert("About Dialog Variations",
+    return alertWithAppIcon("About Dialog Variations",
         "Dialog variations shows several different standard dialog templates for alerts, confirmations, prompts, and more.",
         "For more information, please see:",
         "https://github.com/AdobeXD/Plugin-Samples/tree/master/ui-dialog-variations"
