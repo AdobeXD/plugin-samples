d2a03e0f7ca31360d0a317ff2847c9d19a27d6f4
Ash <ash@ashryanbeats.com>
2019-02-16 12:21:07 -0500

    Updated to use proper UXP form conventions.

--- how-to-export-a-rendition/README.md
+++ how-to-export-a-rendition/README.md
@@ -1,11 +1,11 @@
-# Export artboard as PNG rendition
+# How to export a rendition
 
 This sample generates an export rendition of an object in XD as PNG.
 [Read the step-by-step guide for this sample](https://adobexdplatform.com/plugin-docs/tutorials/how-to-export-a-rendition/).
 
 ## Usage
 
-1. Select an item (artboard)
-1. Run "Plugins > Export Rendition"
-1. Select the filename and location
-1. A UI Modal shows where the file has been saved
+1. Select an object in an XD document
+1. Run _Plugins > How to Export a Rendition_
+1. Select the destination folder from the picker
+1. Get confirmation of save location in a modal dialog
--- how-to-export-a-rendition/main.js
+++ how-to-export-a-rendition/main.js
@@ -2,52 +2,53 @@ const application = require("application");
 const fs = require("uxp").storage.localFileSystem;
 
 async function exportRendition(selection) {
-    if (selection.items.length > 0) {
-        const folder = await fs.getFolder();
-        const file = await folder.createFile("rendition.png");
-        const renditions = [{
-            node: selection.items[0],
-            outputFile: file,
-            type: application.RenditionType.PNG,
-            scale: 2
-        }];
+  if (selection.items.length === 0)
+    return console.log("No selection. Guide the user on what to do.");
 
-        application.createRenditions(renditions)
-            .then(results => {
-                // create the dialog
-                let dialog = document.createElement("dialog");
+  const folder = await fs.getFolder();
+  const file = await folder.createFile("rendition.png", { overwrite: true });
 
-                // main container
-                let container = document.createElement("div");
-                container.style.minWidth = 400;
-                container.style.padding = 40;
-
-                // add content
-                let title = document.createElement("h3");
-                title.style.padding = 20;
-                title.textContent = `PNG Rendition has been saved at ${file.nativePath}`;
-                container.appendChild(title);
-
-                // close button
-                let closeButton = document.createElement("button");
-                closeButton.textContent = "Got it!";
-                container.appendChild(closeButton);
-                closeButton.onclick = (e) => {
-                    dialog.close();
-                }
-
-                document.body.appendChild(dialog);
-                dialog.appendChild(container);
-                dialog.showModal()
-            })
-            .catch(error => {
-                console.log(error);
-            })
+  const renditionOptions = [
+    {
+      node: selection.items[0],
+      outputFile: file,
+      type: application.RenditionType.PNG,
+      scale: 2
     }
+  ];
+
+  await application.createRenditions(renditionOptions);
+  const dialog = createDialog(file.nativePath);
+  return dialog.showModal();
+}
+
+function createDialog(filepath) {
+  document.body.innerHTML = `
+    <style>
+    form {
+        width: 400px;
+    }
+    </style>
+    <dialog id="dialog">
+        <form method="dialog">
+            <h1>Redition saved</h1>
+            <p>Your rendition was saved at:</p>
+            <input type="text" uxp-quiet="true" value="${filepath}" readonly />
+            <footer>
+            <button type="submit" uxp-variant="cta" id="ok-button">OK</button>
+            </footer>
+        </form>
+    </dialog>
+  `;
+
+  const dialog = document.querySelector("#dialog");
+  dialog.addEventListener("close", e => dialog.remove());
+
+  return dialog;
 }
 
 module.exports = {
-    commands: {
-        exportRendition
-    }
+  commands: {
+    exportRendition
+  }
 };
