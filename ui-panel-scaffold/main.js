d375c8b0af0026f10dece6878ae2a0cd10702f50
Ash <ash@ashryanbeats.com>
2019-03-21 14:02:54 -0400

    Restored to simple scaffold.

--- ui-panel-scaffold/main.js
+++ ui-panel-scaffold/main.js
@@ -1,19 +1,7 @@
-const { Artboard, Color, selection } = require("scenegraph");
-
 function show(e) {
   console.log("show");
   console.log(e.node.nodeName);
   console.log(e.node.innerHTML); // Nothing here until you start adding UI!
-
-  e.node.innerHTML = `
-<h1>O Green World!</h1>
-<footer>
-  <button id="omg">Green!</button>
-</footer>
-  `;
-
-  document.querySelector("#omg").addEventListener("click", color);
-  validateSelection();
 }
 function hide(e) {
   console.log("hide");
@@ -22,38 +10,6 @@ function hide(e) {
 }
 function update() {
   console.log("update");
-  validateSelection();
-}
-
-function color() {
-  console.log("color");
-
-  const { executePanelCommand } = require("scenegraph");
-
-  executePanelCommand(() => {
-    console.log("execute");
-
-    const { selection } = require("scenegraph");
-
-    const item = selection.items[0];
-    item.fill = new Color("green");
-  }, "Greened");
-
-  // executePanelCommand(() => {
-  //   console.log(executePanelCommand);
-  //   selection.items = selection.items
-  //     .map(item => item)
-  //     .map(node => (node.fill = new Color("red")));
-  // }, "Create buttons");
-}
-
-function validateSelection() {
-  const { selection } = require("scenegraph");
-  if (selection.items[0]) {
-    document.querySelector("button").disabled = false;
-  } else {
-    document.querySelector("button").disabled = true;
-  }
 }
 
 module.exports = {
