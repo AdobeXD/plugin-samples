d45ec1f9a4eb3fcde8b946ea96efe303bd08799f
Ash <ash@ashryanbeats.com>
2019-05-08 11:11:09 +0200

    Updated to reflect API changes.

--- ui-panel-scaffold/main.js
+++ ui-panel-scaffold/main.js
@@ -8,8 +8,9 @@ function hide(e) {
   console.log(e.node.nodeName);
   console.log(e.node.innerHTML); // Nothing here until you start adding UI!
 }
-function update() {
+function update(selection, documentRoot) {
   console.log("update");
+  console.log(selection.items); // The SceneNodeList of user-selected items.
 }
 
 module.exports = {
